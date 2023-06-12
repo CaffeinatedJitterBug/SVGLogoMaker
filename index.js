46de53e2667f33b7ab7133d09eb4c0dcfb3b9528
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-06-12 15:03:18 -0400

    Added Shape, Circle, Square and Triangle classes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ inquirer
         {
             type: 'input',
             message: 'What color would you like the text to be?',
-            name: 'text-color'
+            name: 'tcolor'
         },
         {
             type: 'input',
@@ -22,10 +22,18 @@ inquirer
         {
             type: 'input',
             message: 'What color would you like for your logo?',
-            name: 'shape-color'
+            name: 'scolor'
         },
     ])
     //Takes the response from the inquirer prompt, deconstructs it, then passes the values to a function in shapes.js
     .then((response) => {
-        
+        const {text, tcolor, shape, scolor} = response;
+
+        if (shape.toLowerCase === 'circle') {
+            shapes.Circle.render(scolor, text, tcolor);
+        } else if (shape.toLowerCase === 'square') {
+            shapes.Square.render(scolor, text, tcolor);
+        } else if (shape.toLowerCase === 'triangle') {
+            shapes.Triangle.render(scolor, text, tcolor);
+        }
     })
