6040eeccf6bde2b4a30886975454eb38375880c7
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-08-18 19:07:26 -0400

    Done!

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SVGLogoMaker
+![License: No License](https://img.shields.io/github/license/CaffeinatedJitterBug/SVGLogoMaker)
+
+## Description
+SVG Logo Maker is a command line program that generates SVG logos based off of user input. Logos generated through this program are very simple, containing one of three shapes (circle, square and triangle), up to three letters of text, and colors for the shape and the text.
+
+## Installation
+Navigate to where you want the project to be saved in your command line and clone the GitHub repository with 'git clone'. Be sure to install Node.js if you don't already have it. Once the repository has been downloaded and Node.js is installed, run 'npm install' or 'npm i' on the repository to install all needed dependencies.
+
+## Usage
+In the command line, you will be prompted to enter information about your logo, such as shape and color. Answer each of the questions, pressing enter after each answer. Once you've answered all the questions, your logo will be generated and saved in the /svgs directory.
+
+## License
+No license has been applied to this project.
--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 //Imports necessary dependancies
-const inquirer = require('inquirer');
-const { readFile, writeFile } = require('fs/promises');
-const Shape = require('./lib/shapes');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { readFile, writeFile } = require("fs/promises");
+const Shape = require("./lib/shapes");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 //Inquirer prompt
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter up to 3 letters to put in your logo',
-            name: 'text'
-        },
-        {
-            type: 'input',
-            message: 'What color would you like the text to be?',
-            name: 'tcolor'
-        },
-        {
-            type: 'input',
-            message: 'What shape do you want your logo to be?',
-            name: 'shapeType'
-        },
-        {
-            type: 'input',
-            message: 'What color would you like for your logo?',
-            name: 'scolor'
-        },
-    ])
-    //Takes the response from the inquirer prompt, deconstructs it, then passes the values to a function in shapes.js
-    .then((response) => {
-        const {text, tcolor, shapeType, scolor} = response;
-        let shapeToLower = shapeType.toLowerCase();
-        let logoShape = '';
+  .prompt([
+    {
+      type: "input",
+      message: "Enter up to 3 letters to put in your logo",
+      name: "text",
+    },
+    {
+      type: "input",
+      message: "What color would you like the text to be?",
+      name: "tcolor",
+    },
+    {
+      type: "input",
+      message: "What shape do you want your logo to be?",
+      name: "shapeType",
+    },
+    {
+      type: "input",
+      message: "What color would you like for your logo?",
+      name: "scolor",
+    },
+  ])
+  //Takes the response from the inquirer prompt, deconstructs it, then passes the values to a function in shapes.js
+  .then((response) => {
+    const { text, tcolor, shapeType, scolor } = response;
+    let shapeToLower = shapeType.toLowerCase();
+    let logoShape = "";
 
-        if (shapeToLower === 'circle') {
-            logoShape = new Circle(scolor, text, tcolor);
-        } else if (shapeToLower === 'square') {
-            logoShape = new Square(scolor, text, tcolor);
-        } else if (shapeToLower === 'triangle') {
-            logoShape = new Triangle(scolor, text, tcolor);
-        } else {
-            console.log("Invalid shape: Please enter either square, circle or triangle")
-        }
+    if (shapeToLower === "circle") {
+      logoShape = new Circle(scolor, text, tcolor).render();
+    } else if (shapeToLower === "square") {
+      logoShape = new Square(scolor, text, tcolor).render();
+    } else if (shapeToLower === "triangle") {
+      logoShape = new Triangle(scolor, text, tcolor).render();
+    } else {
+      console.log("Invalid shape: Please enter either square, circle or triangle");
+    }
 
-        console.log(logoShape);
-        const logoCode = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">' + logoShape + '</svg>';
-        console.log(logoCode);
-    })
+    const logoCode = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n\n' + "  " + logoShape + "\n\n</svg>";
+    const fileName = "./svgs/" + text + ".svg";
+
+    fs.writeFile(fileName, logoCode, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  });
